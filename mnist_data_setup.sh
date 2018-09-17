89719a46d32d4fe6c9531821fbc8d8c7697f6408
khk <khk.dblab@gmail.com>
2018-09-17 09:42:51 +0900

    nifi-academy

--- mnist_data_setup.sh
+++ mnist_data_setup.sh
@@ -2,7 +2,7 @@ spark-submit \
 --master yarn \
 --deploy-mode cluster \
 --num-executors 2 \
---archives mnist/mnist.zip#mnist \
-/${HOME}/nifi-academy/mnist_data_setup.py \
+--archives $(pwd)/mnist/mnist.zip#mnist \
+$(pwd)/mnist_data_setup.py \
 --output mnist/csv2 \
 --format csv2
--- train.sh
+++ train.sh
@@ -4,16 +4,15 @@ spark-submit \
 --queue default \
 --num-executors 2 \
 --executor-memory 1G \
---py-files /root/nifi-academy/tfspark.zip,/root/nifi-academy/TensorFlowOnSpark/examples/mnist/streaming/mnist_dist.py \
+--py-files $(pwd)/TensorFlowOnSpark/examples/mnist/streaming/mnist_dist.py \
 --conf spark.dynamicAllocation.enabled=false \
 --conf spark.yarn.maxAppAttempts=1 \
 --conf spark.streaming.stopGracefullyOnShutdown=true \
 --conf spark.executorEnv.HADOOP_HDFS_HOME=/usr/hdp/current/hadoop-hdfs-client \
 --conf spark.executorEnv.LD_LIBRARY_PATH="$LIB_HDFS:$LIB_JVM" \
 --conf spark.executorEnv.CLASSPATH="$($HADOOP_HOME/bin/hadoop classpath --glob):${CLASSPATH}" \
-/root/nifi-academy/TensorFlowOnSpark/examples/mnist/streaming/mnist_spark.py \
+$(pwd)/TensorFlowOnSpark/examples/mnist/streaming/mnist_spark.py \
 --images stream_data \
 --format csv2 \
---mode inference \
---model mnist_model \
---output predictions/batch
+--mode train \
+--model mnist_model
